0f36e5ea272e8523834eb23ff77c1a7df519a1ff
Remi Thebault <remi.thebault@gmail.com>
2017-09-24 21:00:12 +0200

    rustfmt

--- plugins/amp/src/lib.rs
+++ plugins/amp/src/lib.rs
@@ -5,12 +5,10 @@ extern crate lv2;
 
 use std::f32;
 
-fn db_to_coef(db: f32) -> f32
-{
+fn db_to_coef(db: f32) -> f32 {
     if db > -90f32 {
         10f32.powf(db * 0.05f32)
-    }
-    else {
+    } else {
         0f32
     }
 }
@@ -25,17 +23,16 @@ mod ports {
     });
 }
 
-struct Amp { }
+struct Amp {}
 
-impl<'h> lv2::Plugin<'h> for Amp
-{
-    fn new (_sample_rate: f64, _bundle_path: &str) -> Self {
-        Amp { }
+impl<'h> lv2::Plugin<'h> for Amp {
+    fn new(_sample_rate: f64, _bundle_path: &str) -> Self {
+        Amp {}
     }
 
     fn run(&mut self, ports: &mut Self::Ports, sample_count: usize) {
         let gain = db_to_coef(*ports.gain);
-        for sample in 0 .. sample_count {
+        for sample in 0..sample_count {
             ports.output[sample] = ports.input[sample] * gain;
         }
     }
