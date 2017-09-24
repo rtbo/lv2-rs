dd2f7f916a0cb5d10d892ce1bbd67398de5b6726
Remi Thebault <remi.thebault@gmail.com>
2017-09-24 23:59:54 +0200

    add core uris

--- src/core.rs
+++ src/core.rs
@@ -171,3 +171,97 @@ pub mod meta {
     }
 
 }
+
+
+pub const URI: &'static str = "http://lv2plug.in/ns/lv2core";
+
+pub mod class {
+    pub const ALLPASSPLUGIN:      &'static str = "http://lv2plug.in/ns/lv2core#AllpassPlugin";
+    pub const AMPLIFIERPLUGIN:    &'static str = "http://lv2plug.in/ns/lv2core#AmplifierPlugin";
+    pub const ANALYSERPLUGIN:     &'static str = "http://lv2plug.in/ns/lv2core#AnalyserPlugin";
+    pub const AUDIOPORT:          &'static str = "http://lv2plug.in/ns/lv2core#AudioPort";
+    pub const BANDPASSPLUGIN:     &'static str = "http://lv2plug.in/ns/lv2core#BandpassPlugin";
+    pub const CVPORT:             &'static str = "http://lv2plug.in/ns/lv2core#CVPort";
+    pub const CHORUSPLUGIN:       &'static str = "http://lv2plug.in/ns/lv2core#ChorusPlugin";
+    pub const COMBPLUGIN:         &'static str = "http://lv2plug.in/ns/lv2core#CombPlugin";
+    pub const COMPRESSORPLUGIN:   &'static str = "http://lv2plug.in/ns/lv2core#CompressorPlugin";
+    pub const CONSTANTPLUGIN:     &'static str = "http://lv2plug.in/ns/lv2core#ConstantPlugin";
+    pub const CONTROLPORT:        &'static str = "http://lv2plug.in/ns/lv2core#ControlPort";
+    pub const CONVERTERPLUGIN:    &'static str = "http://lv2plug.in/ns/lv2core#ConverterPlugin";
+    pub const DELAYPLUGIN:        &'static str = "http://lv2plug.in/ns/lv2core#DelayPlugin";
+    pub const DISTORTIONPLUGIN:   &'static str = "http://lv2plug.in/ns/lv2core#DistortionPlugin";
+    pub const DYNAMICSPLUGIN:     &'static str = "http://lv2plug.in/ns/lv2core#DynamicsPlugin";
+    pub const EQPLUGIN:           &'static str = "http://lv2plug.in/ns/lv2core#EQPlugin";
+    pub const ENVELOPEPLUGIN:     &'static str = "http://lv2plug.in/ns/lv2core#EnvelopePlugin";
+    pub const EXPANDERPLUGIN:     &'static str = "http://lv2plug.in/ns/lv2core#ExpanderPlugin";
+    pub const EXTENSIONDATA:      &'static str = "http://lv2plug.in/ns/lv2core#ExtensionData";
+    pub const FEATURE:            &'static str = "http://lv2plug.in/ns/lv2core#Feature";
+    pub const FILTERPLUGIN:       &'static str = "http://lv2plug.in/ns/lv2core#FilterPlugin";
+    pub const FLANGERPLUGIN:      &'static str = "http://lv2plug.in/ns/lv2core#FlangerPlugin";
+    pub const FUNCTIONPLUGIN:     &'static str = "http://lv2plug.in/ns/lv2core#FunctionPlugin";
+    pub const GATEPLUGIN:         &'static str = "http://lv2plug.in/ns/lv2core#GatePlugin";
+    pub const GENERATORPLUGIN:    &'static str = "http://lv2plug.in/ns/lv2core#GeneratorPlugin";
+    pub const HIGHPASSPLUGIN:     &'static str = "http://lv2plug.in/ns/lv2core#HighpassPlugin";
+    pub const INPUTPORT:          &'static str = "http://lv2plug.in/ns/lv2core#InputPort";
+    pub const INSTRUMENTPLUGIN:   &'static str = "http://lv2plug.in/ns/lv2core#InstrumentPlugin";
+    pub const LIMITERPLUGIN:      &'static str = "http://lv2plug.in/ns/lv2core#LimiterPlugin";
+    pub const LOWPASSPLUGIN:      &'static str = "http://lv2plug.in/ns/lv2core#LowpassPlugin";
+    pub const MIXERPLUGIN:        &'static str = "http://lv2plug.in/ns/lv2core#MixerPlugin";
+    pub const MODULATORPLUGIN:    &'static str = "http://lv2plug.in/ns/lv2core#ModulatorPlugin";
+    pub const MULTIEQPLUGIN:      &'static str = "http://lv2plug.in/ns/lv2core#MultiEQPlugin";
+    pub const OSCILLATORPLUGIN:   &'static str = "http://lv2plug.in/ns/lv2core#OscillatorPlugin";
+    pub const OUTPUTPORT:         &'static str = "http://lv2plug.in/ns/lv2core#OutputPort";
+    pub const PARAEQPLUGIN:       &'static str = "http://lv2plug.in/ns/lv2core#ParaEQPlugin";
+    pub const PHASERPLUGIN:       &'static str = "http://lv2plug.in/ns/lv2core#PhaserPlugin";
+    pub const PITCHPLUGIN:        &'static str = "http://lv2plug.in/ns/lv2core#PitchPlugin";
+    pub const PLUGIN:             &'static str = "http://lv2plug.in/ns/lv2core#Plugin";
+    pub const PLUGINBASE:         &'static str = "http://lv2plug.in/ns/lv2core#PluginBase";
+    pub const POINT:              &'static str = "http://lv2plug.in/ns/lv2core#Point";
+    pub const PORT:               &'static str = "http://lv2plug.in/ns/lv2core#Port";
+    pub const PORTPROPERTY:       &'static str = "http://lv2plug.in/ns/lv2core#PortProperty";
+    pub const RESOURCE:           &'static str = "http://lv2plug.in/ns/lv2core#Resource";
+    pub const REVERBPLUGIN:       &'static str = "http://lv2plug.in/ns/lv2core#ReverbPlugin";
+    pub const SCALEPOINT:         &'static str = "http://lv2plug.in/ns/lv2core#ScalePoint";
+    pub const SIMULATORPLUGIN:    &'static str = "http://lv2plug.in/ns/lv2core#SimulatorPlugin";
+    pub const SPATIALPLUGIN:      &'static str = "http://lv2plug.in/ns/lv2core#SpatialPlugin";
+    pub const SPECIFICATION:      &'static str = "http://lv2plug.in/ns/lv2core#Specification";
+    pub const SPECTRALPLUGIN:     &'static str = "http://lv2plug.in/ns/lv2core#SpectralPlugin";
+    pub const UTILITYPLUGIN:      &'static str = "http://lv2plug.in/ns/lv2core#UtilityPlugin";
+    pub const WAVESHAPERPLUGIN:   &'static str = "http://lv2plug.in/ns/lv2core#WaveshaperPlugin";
+}
+pub mod prop {
+    pub const APPLIESTO:          &'static str = "http://lv2plug.in/ns/lv2core#appliesTo";
+    pub const BINARY:             &'static str = "http://lv2plug.in/ns/lv2core#binary";
+    pub const DEFAULT:            &'static str = "http://lv2plug.in/ns/lv2core#default";
+    pub const DESIGNATION:        &'static str = "http://lv2plug.in/ns/lv2core#designation";
+    pub const DOCUMENTATION:      &'static str = "http://lv2plug.in/ns/lv2core#documentation";
+    pub const EXTENSIONDATA:      &'static str = "http://lv2plug.in/ns/lv2core#extensionData";
+    pub const FREEWHEELING:       &'static str = "http://lv2plug.in/ns/lv2core#freeWheeling";
+    pub const INDEX:              &'static str = "http://lv2plug.in/ns/lv2core#index";
+    pub const INTEGER:            &'static str = "http://lv2plug.in/ns/lv2core#integer";
+    pub const LATENCY:            &'static str = "http://lv2plug.in/ns/lv2core#latency";
+    pub const MAXIMUM:            &'static str = "http://lv2plug.in/ns/lv2core#maximum";
+    pub const MICROVERSION:       &'static str = "http://lv2plug.in/ns/lv2core#microVersion";
+    pub const MINIMUM:            &'static str = "http://lv2plug.in/ns/lv2core#minimum";
+    pub const MINORVERSION:       &'static str = "http://lv2plug.in/ns/lv2core#minorVersion";
+    pub const NAME:               &'static str = "http://lv2plug.in/ns/lv2core#name";
+    pub const OPTIONALFEATURE:    &'static str = "http://lv2plug.in/ns/lv2core#optionalFeature";
+    pub const PORT:               &'static str = "http://lv2plug.in/ns/lv2core#port";
+    pub const PORTPROPERTY:       &'static str = "http://lv2plug.in/ns/lv2core#portProperty";
+    pub const PROJECT:            &'static str = "http://lv2plug.in/ns/lv2core#project";
+    pub const PROTOTYPE:          &'static str = "http://lv2plug.in/ns/lv2core#prototype";
+    pub const REQUIREDFEATURE:    &'static str = "http://lv2plug.in/ns/lv2core#requiredFeature";
+    pub const SCALEPOINT:         &'static str = "http://lv2plug.in/ns/lv2core#scalePoint";
+    pub const SYMBOL:             &'static str = "http://lv2plug.in/ns/lv2core#symbol";
+}
+pub mod inst {
+    pub const CONNECTIONOPTIONAL: &'static str = "http://lv2plug.in/ns/lv2core#connectionOptional";
+    pub const CONTROL:            &'static str = "http://lv2plug.in/ns/lv2core#control";
+    pub const ENUMERATION:        &'static str = "http://lv2plug.in/ns/lv2core#enumeration";
+    pub const HARDRTCAPABLE:      &'static str = "http://lv2plug.in/ns/lv2core#hardRTCapable";
+    pub const INPLACEBROKEN:      &'static str = "http://lv2plug.in/ns/lv2core#inPlaceBroken";
+    pub const ISLIVE:             &'static str = "http://lv2plug.in/ns/lv2core#isLive";
+    pub const REPORTSLATENCY:     &'static str = "http://lv2plug.in/ns/lv2core#reportsLatency";
+    pub const SAMPLERATE:         &'static str = "http://lv2plug.in/ns/lv2core#sampleRate";
+    pub const TOGGLED:            &'static str = "http://lv2plug.in/ns/lv2core#toggled";
+}
